e9dc21db953695dd6d3288f2c9e0c3bb7f9871eb
zestypratyush <pranjalpratyush69@gmail.com>
2022-10-16 03:31:03 +0530

    Created Message and User schema

---
+++ backend/models/UserModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const UserModel = mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    pic: {
+        type: String,
+        required: true,
+        default: "https://cdn-icons-png.flaticon.com/512/1160/1160040.png?w=360",
+    },
+},{
+    timestamps: true
+})
+
+const User = mongoose.model("User", UserModel);
+module.exports = User;
