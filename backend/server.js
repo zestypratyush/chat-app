a7d6c878c2bb3c7c1fc6c0b07f9b86d1d64d5805
zestypratyush <pranjalpratyush69@gmail.com>
2022-10-15 23:30:47 +0530

    Created express routes for single and group chat

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,22 @@
 const express = require("express");
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
-app.listen(5000, console.log("Server started on port 5000"));
+app.get("/", (req,res) => {
+    res.send("API is running");
+});
+
+app.get("/api/chat", (req,res)=> {
+    res.send(chats);
+});
+
+app.get("/api/chat/:id", (req,res) => {
+    const singleChat = chats.find((c) => c._id=== req.params.id);
+    res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
