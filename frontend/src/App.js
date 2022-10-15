756dc6416137ad90f792145770ec7f53b8755072
zestypratyush <pranjalpratyush69@gmail.com>
2022-10-16 00:11:25 +0530

    Created react app and chakra ui

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { Button  } from '@chakra-ui/react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">  
+    <Button colorScheme='blue'>Button</Button>
+    </div>
+  );
+}
+
+export default App;
