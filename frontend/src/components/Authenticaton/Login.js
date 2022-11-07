581940fc05a338fbfb144d7e4cb395d30cfaef7f
zestypratyush <pranjalpratyush69@gmail.com>
2022-11-08 00:54:49 +0530

    Completed login ui

--- frontend/src/components/Authenticaton/Login.js
+++ frontend/src/components/Authenticaton/Login.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, Show } from '@chakra-ui/react'
+import { VStack } from '@chakra-ui/react'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    const [show, setShow] = useState(false) 
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState() 
+
+    const handleClick = () => setShow(!show) ; 
+
+    const submitHandler = () => {};
+
+  return <VStack spacing='5px'> 
+
+    <FormControl id='email' isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+            placeholder=' Enter your Email '
+            onChange={(e)=> setEmail(e.target.value)}
+        /> 
+    </FormControl>
+
+    <FormControl id='password' isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+        <Input
+            type={show? "text":"password"}
+            placeholder=' Enter your Password '
+            onChange={(e)=> setPassword(e.target.value)}
+        /> 
+        <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+                {show? "Hide" : "Show"}
+            </Button>
+        </InputRightElement>
+        </InputGroup>
+    </FormControl>
+ 
+    <Button 
+        colorScheme={"blue"}
+        width= "100%"
+        style={{marginTop: 15}}
+        onClick= {submitHandler}
+    >
+        Login 
+    </Button>
+
+    <Button 
+        variant={"solid"}
+        colorScheme={"red"}
+        width= "100%"
+        style={{marginTop: 15}}
+        onClick= {() =>{
+            setEmail("guest@example.com");
+            setPassword("123456");
+        }}
+    >
+        Get Guest User Credentials 
+    </Button>
+  </VStack>;
 }
 
 export default Login
