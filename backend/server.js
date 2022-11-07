3371b7653a366fbc639f1edc9fc00221e252e7b3
zestypratyush <pranjalpratyush69@gmail.com>
2022-11-08 01:34:58 +0530

    Connected mongoDB atlas

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { chats } = require("./data/data");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
-const app = express();
 dotenv.config();
+connectDB();
+const app = express();
 
 app.get("/", (req,res) => {
     res.send("API is running");
@@ -19,4 +22,4 @@ app.get("/api/chat/:id", (req,res) => {
 });
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, console.log(`Server started on port ${PORT}`.yellow.bold));
