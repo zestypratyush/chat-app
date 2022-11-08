08a3c1130337c10a7edae22b93fbbcbfd0065584
zestypratyush <pranjalpratyush69@gmail.com>
2022-11-08 11:04:10 +0530

    User registration on database

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose');
 
 const UserModel = mongoose.Schema({
     name: {type: String, required: true},
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     pic: {
-        type: String,
-        required: true,
+        type: String, 
         default: "https://cdn-icons-png.flaticon.com/512/1160/1160040.png?w=360",
     },
 },{
