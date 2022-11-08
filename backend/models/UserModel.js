a5ed53f73d4621af64ab1c1bcb4ab760cb397e75
zestypratyush <pranjalpratyush69@gmail.com>
2022-11-08 12:24:32 +0530

    JWT Authentication and bcryptjs hashing

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserModel = mongoose.Schema({
     name: {type: String, required: true},
@@ -10,6 +11,19 @@ const UserModel = mongoose.Schema({
     },
 },{
     timestamps: true
+})
+
+UserModel.methods.matchPassword = async function (enteredPass){
+    return await bcrypt.compare(enteredPass, this.password);
+}
+
+UserModel.pre('save', async function (next){
+    if(!this.isModified){
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 })
 
 const User = mongoose.model("User", UserModel);
