0a47c58eb62f1960d9ea1490bd862cda0415f56a
zestypratyush <pranjalpratyush69@gmail.com>
2022-11-08 12:24:04 +0530

    JWT Authentication and bcryptjs hashing

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -39,4 +39,23 @@ const registerUser = asyncHandler(async(req, res) => {
     }
 });
 
-module.exports = {registerUser};
+const authUser = asyncHandler(async(req, res) =>{
+    const {email, password} = req.body;
+
+    const user = await User.findOne({ email });
+
+    if(user && (await user.matchPassword(password))){
+        res.json({
+            _id: user._id,
+            name: user.name, 
+            email: user.email, 
+            pic: user.pic,
+            token: generateToken(user._id),
+        });
+    }else{
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
+});
+
+module.exports = {registerUser, authUser};
