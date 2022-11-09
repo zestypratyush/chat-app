824ff6f7417a4e4f0491800d2807d7dfa100f902
zestypratyush <pranjalpratyush69@gmail.com>
2022-11-09 16:03:31 +0530

    Created API for fetching all chats of user

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -76,41 +76,4 @@ const fetchChats = asyncHandler(async (req, res) => {
   });
 
 
-//@description     Create New Group Chat
-//@route           POST /api/chat/group
-//@access          Protected
-const createGroupChat = asyncHandler(async (req, res) => {
-    if (!req.body.users || !req.body.name) {
-      return res.status(400).send({ message: "Please Fill all the feilds" });
-    }
-  
-    var users = JSON.parse(req.body.users);
-  
-    if (users.length < 2) {
-      return res
-        .status(400)
-        .send("More than 2 users are required to form a group chat");
-    }
-  
-    users.push(req.user);
-  
-    try {
-      const groupChat = await Chat.create({
-        chatName: req.body.name,
-        users: users,
-        isGroupChat: true,
-        groupAdmin: req.user,
-      });
-  
-      const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
-        .populate("users", "-password")
-        .populate("groupAdmin", "-password");
-  
-      res.status(200).json(fullGroupChat);
-    } catch (error) {
-      res.status(400);
-      throw new Error(error.message);
-    }
-  });
-
-  module.exports = {accessChat, fetchChats, createGroupChat};
+module.exports = {accessChat, fetchChats };
