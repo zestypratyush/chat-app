7b588ab0db6aa8be732c00206cf6610d9759f016
zestypratyush <pranjalpratyush69@gmail.com>
2022-11-09 15:40:28 +0530

    Created API for one on one chat

---
+++ backend/routes/chatRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {protect} = require("../middleware/authMiddleware");
+const {accessChat} = require("../controllers/chatControllers")
+
+const router= express.Router();
+
+router.route("/").post(protect,accessChat);
+// router.route("/").get(protect,fetchChats);
+// router.route("/group").post(protect,CreateGroupChat);
+// router.route("/rename").put(protect,renameGroup);
+// router.route("/groupremove").put(protect, removeFromGroup);
+// router.route("/groupadd").put(protect,addToGroup);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const {notFound, errorHandler} = require('./middleware/errorMiddleware');
 
 dotenv.config();
@@ -16,16 +17,8 @@ app.get("/", (req,res) => {
     res.send("API is running successfully");
 });
 
-// app.get("/api/chat", (req,res)=> {
-//     res.send(chats);
-// });
-
-// app.get("/api/chat/:id", (req,res) => {
-//     const singleChat = chats.find((c) => c._id=== req.params.id);
-//     res.send(singleChat);
-// });
-
 app.use('/api/user', userRoutes);
+app.use('/api/chat',chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
